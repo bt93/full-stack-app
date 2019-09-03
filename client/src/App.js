b229b7eea42f5660211f7f0b120ccedb35597a13
Jason Howie <howiejason93@gmail.com>
2019-09-03 11:33:00 -0400

    add links and finish home page

--- client/src/App.js
+++ client/src/App.js
@@ -1,18 +1,33 @@
 import React, { Component } from 'react';
+import { 
+  BrowserRouter as Router, 
+  Route,
+  Redirect,
+  Switch
+} from 'react-router-dom';
+
+// Component imports
+import Header from './components/Header';
+import Courses from './components/Courses';
+
+// Style imports
 import './styles/global.css';
 
 
 class App extends Component {
-  state = {
-    data: null,
-    isLoading: true
-  }
 
   render() {
     return (
-      <div className="App">
-        
-      </div>
+      <Router>
+        <div className="App">
+          <Header />
+          <hr />
+
+          <Switch>
+            <Route path={"/"} component={Courses}/>
+          </Switch>
+        </div>
+      </Router>
     );
     }
 }
---
+++ client/src/components/Courses.js
@@ -0,0 +1,54 @@
+import React, { Component } from 'react';
+import { Link } from "react-router-dom";
+
+
+class Courses extends Component {
+    state = {
+        data: null,
+        isLoading: true
+    }
+
+    componentDidMount() {
+        fetch('http://localhost:5000/api/courses')
+            .then(res => res.json())
+            .then(res => this.setState(prevState => {
+                return {
+                data: res.courses,
+                isLoading: false
+            }
+        }));
+    }
+
+    render() {
+        let renderedData;
+
+        if (!this.state.isLoading) {
+            if (this.state.data.length > 0) {
+                renderedData = this.state.data.map(course => {
+                    return (
+                        <div key={course.id} className="grid-33"><Link className="course--module course--link" to={`/course/${course.id}`}>
+                            <h4 className="course--label">Course</h4>
+                            <h3 className="course--title">{course.title}</h3>
+                        </Link></div>
+                    )
+                });
+            }
+        } else {
+            renderedData = <h1>Loading...</h1>
+        }
+
+        return (
+            <div className="bounds">
+                {renderedData}
+                <div className="grid-33"><Link className="course--module course--add--module" to="/createcourse">
+                    <h3 className="course--add--title"><svg version="1.1" xmlns="http://www.w3.org/2000/svg" x="0px" y="0px"
+                        viewBox="0 0 13 13" className="add">
+                        <polygon points="7,6 7,0 6,0 6,6 0,6 0,7 6,7 6,13 7,13 7,7 13,7 13,6 "></polygon>
+                    </svg>New Course</h3>
+                </Link></div>
+            </div>
+        )
+    }
+}
+
+export default Courses;
