162aedc416924364f0f0e2d1c6f0d6eb2d9dbf0d
Jason Howie <howiejason93@gmail.com>
2019-09-17 09:42:21 -0400

    Updates

---
+++ precache-manifest.ebd7b9c03ff262bb6a24f7a8a1f5963c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e129e004ed0d042c8266c28324dd56ed",
+    "url": "/full-stack-api/index.html"
+  },
+  {
+    "revision": "40a9dfd85b9f7cadb4ce",
+    "url": "/full-stack-api/static/css/main.1c6fd386.chunk.css"
+  },
+  {
+    "revision": "7644785ef4a0613e4221",
+    "url": "/full-stack-api/static/js/2.6a9ff814.chunk.js"
+  },
+  {
+    "revision": "40a9dfd85b9f7cadb4ce",
+    "url": "/full-stack-api/static/js/main.7e365826.chunk.js"
+  },
+  {
+    "revision": "8263c4b9b4cdacfe519e",
+    "url": "/full-stack-api/static/js/runtime~main.46a0e077.js"
+  },
+  {
+    "revision": "26ef8bb418031b9bb4f44e1aeea71186",
+    "url": "/full-stack-api/static/media/loading.26ef8bb4.gif"
+  }
+]);
